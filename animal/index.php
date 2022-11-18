56d6235f159679539ec613be51e27f31a4049ddc
Marko <marko.dzomba.info@gmail.com>
2022-11-18 21:43:52 +0100

    animal index

--- animal/index.php
+++ animal/index.php
@@ -56,8 +56,8 @@ mysqli_close($connect);
     <?php require_once '../components/boot.php' ?>
     <style type="text/css">
     .img-thumbnail {
-        width: 70px !important;
-        height: 70px !important;
+        width: 150px !important;
+        height: 150px !important;
     }
 
     td {
@@ -70,14 +70,11 @@ mysqli_close($connect);
     }
 
     .userImage {
-        width: 100px;
+        width: 150px;
         height: auto;
     }
     .hero {
-background: #4c4c4c; 
-background: -moz-linear-gradient(top,  #4c4c4c 0%, #595959 12%, #666666 25%, #474747 39%, #2c2c2c 50%, #000000 51%, #111111 60%, #2b2b2b 76%, #1c1c1c 91%, #131313 100%);
-background: -webkit-linear-gradient(top,  #4c4c4c 0%,#595959 12%,#666666 25%,#474747 39%,#2c2c2c 50%,#000000 51%,#111111 60%,#2b2b2b 76%,#1c1c1c 91%,#131313 100%); 
-background: linear-gradient(to bottom,  #4c4c4c 0%,#595959 12%,#666666 25%,#474747 39%,#2c2c2c 50%,#000000 51%,#111111 60%,#2b2b2b 76%,#1c1c1c 91%,#131313 100%);
+background-color: beige;
 
     }
     </style>
@@ -85,25 +82,6 @@ background: linear-gradient(to bottom,  #4c4c4c 0%,#595959 12%,#666666 25%,#4747
 
 <body>
 
-<div class="hero p-4 mb-3 text-center">
-    <img class="userImage" src="../picture/admavatar.png" alt="admavatar">
-    <p class="text-warning">Administrator <?php echo $rowd['first_name'] . " " . $rowd['last_name']; ?></p>
-</div>
-
-    <div class="manageProduct w-75 mt-3">
-        <div class='m-4'>
-            <a href="create.php"><button class='btn btn-primary' type="button">Add </button></a>
-            <a href="../dashboard.php"><button class='btn btn-success' type="button">Dashboard</button></a>
-        </div>
-    </div>
-    <div class="container">
-        <p class='h2'>Animal</p>
-            <div class="container">
-                <div class="row">
-                <?= $tbody; ?>
-                </div>
-            </div>
-    </div>
 </body>
 
 </html>
