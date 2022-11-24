b7d3e5825481dac0e49532ad0ded82af022b83e3
Marko <marko.dzomba.info@gmail.com>
2022-11-24 16:33:40 +0100

    registration form

--- register.php
+++ register.php
@@ -102,47 +102,59 @@ mysqli_close($connect);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login & Registration</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+
     <?php require_once 'components/boot.php' ?>
 
     <style>
-         body{
-            background-image: url("./picture/doghero1")
+        body {
+            background-image: url("picture/cat13.jpg");
+        }
+
+        .container {
+            margin-top: 7%;
+            
+        }
+
+        .register {
+            color: gray;
+            font-family: Nunito;
         }
     </style>
 </head>
 
 <body>
-    <div class="container border rounded border-dark p-2 w-50 mt-2">
+    <div class="cont1 container border rounded border-dark p-2 w-50">
         <form class="w-100" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off" enctype="multipart/form-data">
-            <h2>Registration Form</h2>
+            <h2 class="register">Registration Form</h2>
             <hr />
             <?php
             if (isset($errMSG)) {
             ?>
-            <div class="alert alert-<?php echo $errTyp ?>">
-                <p><?php echo $errMSG; ?></p>
-                <p><?php echo $uploadError; ?></p>
-            </div>
+                <div class="alert alert-<?php echo $errTyp ?>">
+                    <p><?php echo $errMSG; ?></p>
+                    <p><?php echo $uploadError; ?></p>
+                </div>
 
             <?php
             }
             ?>
 
-            <input type="text" name="first_name" class="form-control" placeholder="First Name" maxlength="50" value="<?php echo $fname ?>" />
+            <input type="text" name="first_name" class="form-control mb-1" placeholder="First Name" maxlength="50" value="<?php echo $fname ?>" />
             <span class="text-danger"> <?php echo $fnameError; ?> </span>
 
-            <input type="text" name="last_name" class="form-control" placeholder="Last Name" maxlength="50" value="<?php echo $lname ?>" />
+            <input type="text" name="last_name" class="form-control mb-1" placeholder="Last Name" maxlength="50" value="<?php echo $lname ?>" />
             <span class="text-danger"> <?php echo $fnameError; ?> </span>
 
-            <input type="email" name="email" class="form-control" placeholder="E-mail" maxlength="40" value="<?php echo $email ?>" />
+            <input type="email" name="email" class="form-control mb-1" placeholder="E-mail" maxlength="40" value="<?php echo $email ?>" />
             <span class="text-danger"> <?php echo $emailError; ?> </span>
             <div class="d-flex">
-                <input class='form-control w-100' type="file" name="picture">
+                <input class='form-control w-100 mb-1' type="file" name="picture">
                 <span class="text-danger"> <?php echo $picError; ?> </span>
             </div>
-            <input type="password" name="password" class="form-control" placeholder="Password" maxlength="15" />
+            <input type="password" name="password" class="form-control mb-1" placeholder="Password" maxlength="15" />
             <span class="text-danger"> <?php echo $passError; ?> </span>
-            <input type="text" name="address" class="form-control" placeholder="Address" maxlength="100" value="<?php echo $address ?>" />
+            <input type="text" name="address" class="form-control mb-1" placeholder="Address" maxlength="100" value="<?php echo $address ?>" />
             <input type="number" name="phone_number" class="form-control" placeholder="Phone Number" maxlength="20" value="<?php echo $phone_number ?>" />
             <hr />
             <button type="submit" class="btn btn-block btn-primary" name="btn-signup">Register</button>
